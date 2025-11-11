b4fdf32a53862a4097a3df080621e966381f6842
Izeta <izeta.julen@uni.eus>
2025-11-11 09:37:54 +0100

    feat: replace hardcoded credentials by .env variables.

--- config.php
+++ config.php
@@ -1,17 +1,10 @@
 <?php
 
 // Datu baserako konexiorako aldagai globalak konfiguratu
-
-// DEV
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'zubiri_manteo');
-
-// PROD
-// define('DB_HOST', '192.168.1.105');
-// define('DB_USER', 'mysql_usu');
-// define('DB_PASS', 'Admin123');
-// define('DB_NAME', 'zubiri');
+// Ez badago .env-rik garapenerako balioak ezarriko dira 
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASS') ?: '');
+define('DB_NAME', getenv('DB_NAME') ?: 'zubiri_manteo');
 
 ?>
