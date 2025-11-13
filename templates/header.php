8f59814fc6e6f9dfeb9c6d0549ce8d9ace106df5
Izeta <izeta.julen@uni.eus>
2025-11-13 10:22:16 +0100

    feat: headerra refaktorizatu enpresa berrira.

--- templates/header.php
+++ templates/header.php
@@ -20,7 +20,7 @@ define('BASE_URL', $protocol . $host . $script_name);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo htmlspecialchars($orri_titulua . ' - IES Xabier Zubiri Manteo BHI' ?? 'IES Xabier Zubiri Manteo BHI'); ?></title>
+    <title><?php echo htmlspecialchars($orri_titulua . ' - Unanue Upategiak' ?? 'Unanue Upategiak'); ?></title>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
@@ -28,7 +28,7 @@ define('BASE_URL', $protocol . $host . $script_name);
 
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/style.css">
     
-    <link rel="shortcut icon" href="<?php echo BASE_URL; ?>public/img/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo BASE_URL; ?>public/img/favicon.ico" type="image/x-icon">
 </head>
 <body class="d-flex flex-column min-vh-100">
 
@@ -37,7 +37,7 @@ define('BASE_URL', $protocol . $host . $script_name);
         <div class="container">
             <a class="navbar-brand" href="<?php echo BASE_URL; ?>index.php">
                 <img src="<?php echo BASE_URL; ?>public/img/logo.png" alt="Logo" style="height: 40px;">
-                Zubiri Manteo
+                Unanue Upategiak
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -48,20 +48,20 @@ define('BASE_URL', $protocol . $host . $script_name);
                         <a class="btn btn-light" href="<?php echo BASE_URL; ?>index.php">Hasiera</a>
                     </li>
                     <?php 
-                        // Ikaslearen aukerak administratzaileak bakarrik ditu.
+                        // Administratzailearen aukerak
                         if (isset($_SESSION['is_admin']) && $_SESSION['is_admin']) {
-                            echo '<li class="nav-item me-2"><a class="btn btn-light" href="' . BASE_URL . 'ikasle-zerrenda.php">Ikasle Zerrenda</a></li>';
-                            echo '<li class="nav-item"><a class="btn btn-light" href="' . BASE_URL . 'ikaslea-sortu.php">Ikaslea Sortu</a></li>';
+                            echo '<li class="nav-item me-2"><a class="btn btn-light" href="' . BASE_URL . 'langile-zerrenda.php">Langile Zerrenda</a></li>';
+                            echo '<li class="nav-item me-2"><a class="btn btn-light" href="' . BASE_URL . 'langilea-sortu.php">Langilea Sortu</a></li>';
+                            echo '<li class="nav-item me-2"><a class="btn btn-light" href="' . BASE_URL . 'oharrak-kudeatu.php">Oharrak Kudeatu</a></li>';
                         }
                     ?>
                 </ul>
                 <div class="d-flex align-items-center">
-                    <?php if (isset($_SESSION['erabiltzailea'])): ?>
-                        <span class="navbar-text me-3">Kaixo, <?php echo htmlspecialchars($_SESSION['erabiltzailea']); ?></span>
+                    <?php if (isset($_SESSION['username'])): ?>
+                        <span class="navbar-text me-3">Kaixo, <?php echo htmlspecialchars($_SESSION['username']); ?></span>
                         <a href="<?php echo BASE_URL; ?>itxi-saioa.php" class="btn btn-outline-primary">Saioa Itxi</a>
                     <?php else: ?>
                         <a href="<?php echo BASE_URL; ?>hasi-saioa.php" class="btn btn-primary me-2">Hasi Saioa</a>
-                        <a href="<?php echo BASE_URL; ?>kontua-sortu.php" class="btn btn-secondary">Sortu Kontua</a>
                     <?php endif; ?>
                 </div>
             </div>
