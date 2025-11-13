43695c7b7dac126298020b923e4e397e3542549e
Izeta <izeta.julen@uni.eus>
2025-11-13 10:20:35 +0100

    feat: change .env variable names, handle error display and activate httpOnly

--- bootstrap.php
+++ bootstrap.php
@@ -3,39 +3,33 @@
 // Composer autoloader-a kargatu
 require_once __DIR__ . '/vendor/autoload.php';
 
-// Kargatu beharreko .env fitxategia zehaztu
-$envFile = $_ENV['APP_ENV'] === "production" ? '.env.prod' : '.env.dev';
-
-// Dagokion .env fitxategia kargatu
+// .env fitxategia kargatu
 try {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, $envFile);
-    $dotenv->load();
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->safeLoad();
 
     // Behar ditugun konstanteak definitu
-    define('DB_HOST', 'db'); // Docker sareko zerbitzuaren izena
-    define('DB_USER', $_ENV['DB_USER'] ?? null);
-    define('DB_PASS', $_ENV['DB_PASS'] ?? null);
-    define('DB_NAME', $_ENV['DB_NAME'] ?? null);
-    define('APP_ENV', $_ENV['APP_ENV'] ?? 'production');
+    define('DB_HOST', $_ENV['DB_HOST'] ?? null);
+    define('DB_USER', $_ENV['MYSQL_USER'] ?? null);
+    define('DB_PASS', $_ENV['MYSQL_PASSWORD'] ?? null);
+    define('DB_NAME', $_ENV['MYSQL_DATABASE'] ?? null);
+    define('APP_ENV', $_ENV['APP_ENV'] ?? 'development'); // 'development' lehenetsi gisa
 
 } catch (Exception $e) {
     // Gelditu exekuzioa .env fitxategia kargatu ezin bada
     die('Errorea: Ezin izan da konfigurazio fitxategia kargatu. ' . $e->getMessage());
 }
 
-// Ezkutatu oharrak eta erroreak produkziorako
-if ($_ENV['APP_ENV'] === "production") {
+// Ezkutatu oharrak eta erroreak produkziorako, bestela erakutsi denak
+if (APP_ENV === "production") {
     error_reporting(0);
     ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
 } else {
-    error_reporting(1);
+    error_reporting(E_ALL);
     ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
 }
-
-// Erakutsi errore guztiak
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 // Cookien konfigurazioa
 session_set_cookie_params([
@@ -43,7 +37,7 @@ session_set_cookie_params([
     'path' => '/',
     'domain' => '',
     'secure' => !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off', // True HTTPS badago
-    'httponly' => false, // <-- httpOnly desaktibatuta
+    'httponly' => true, // <-- httpOnly aktibatuta
     'samesite' => 'Lax'
 ]);
 
