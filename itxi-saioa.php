257bf618a2c947672fe843914f96d3ca9b9f4209
Izeta <izeta.julen@uni.eus>
2025-11-13 10:24:41 +0100

    feat: refactor logout page.

--- itxi-saioa.php
+++ itxi-saioa.php
@@ -1,7 +1,7 @@
 <?php
-session_start();
-session_unset();
-session_destroy();
-header("Location: index.php");
+require_once 'bootstrap.php'; // Saioa hasi eta konfigurazioak kargatu
+session_unset(); // Saio-aldagai guztiak ezabatu
+session_destroy(); // Saioa suntsitu
+header("Location: hasi-saioa.php"); // Saioa hasteko orrira birbideratu
 exit;
 ?>
